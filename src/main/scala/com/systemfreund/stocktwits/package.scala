61f4415e53cbd593eba904ba5bc0fd40a1c6fd8a
Ömer Yildiz <oemer@systemfreund.com>
2014-05-12 20:16:24 +0200

    fix typo

--- src/main/scala/com/systemfreund/stocktwits/package.scala
+++ src/main/scala/com/systemfreund/stocktwits/package.scala
@@ -21,7 +21,7 @@ package object stocktwits {
   /**
    * Type parameter is needed in order to avoid having to call [[com.systemfreund.stocktwits.Stream]] like this:
    * {{{Stream[SymbolStreamResponse](Symbol("GOOGL"))}}}
-   * Instead, we can write {{{Stream(Symbol("GOOGL")}}} because the compiler can now infer the type parameter.
+   * Instead, we can write {{{Stream(Symbol("GOOGL"))}}} because the compiler can now infer the type parameter.
    */
   sealed class StreamEntity[A](private val path: Path = Empty) {
     private[stocktwits] lazy val uri = Uri((streamsBasePath ++ path + ".json").toString())
